c000246288d297c705a2c315e4025e58342b1bfd
Void&Null <kosmix1999@gmail.com>
2023-01-09 12:26:50 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,9 +3,9 @@ namespace SniperAimbot;
 public static class ModHelperData
 {
     public const string Name = "TF2 Aimbot Sniper";
-    public const string Description = "Get Gud, Get this tower mod.";
+    public const string Description = "Get Gud, Get this tower mod.  Disable Voice lines in the mod menu if you don't like swearing.";
     public const string Version = "1.0.0";
-    public const string RepoName = "TF2-Aimbot-Sniper";
+    public const string RepoName = "BTD6-TF2-Aimbot-Tower";
     public const string RepoOwner = "Void-n-Null";
     public const string DllName = "BananaFarmer.dll";
     public const string Author = "Void & Null";
